9dcce13c800a82e45c9d329ace677ad95084a85b
racio <85686582+RacioTrajano@users.noreply.github.com>
2021-06-22 18:17:22 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+
+int ver_area (int x0, int y0, int x1, int y1, int x2, int y2, int x3, int y3){
+    
+  if (x3 >= x0 && x2<= x1 && y3>= y0 && y2 <= y1) {
+    return 1;
+}
+  else return 0;}
+
+int main () {
+
+int qtd_leituras;
+int x0, y0, x1, y1, x2, y2, x3, y3;
+scanf("%d %d %d %d", &x0, &x1, &y0, &y1);
+
+scanf("%d", &qtd_leituras);
+  for (int i=0; i< qtd_leituras; i++){
+    scanf("%d %d %d %d", &x2, &y2, &x3, &y3);
+    ver_area(x0, y0, x1, y1, x2, y2, x3, y3);
+    printf("%d", ver_area(x0, y0, x1, y1, x2, y2, x3, y3));
+  }
+
+
+
+  return 0;
+}
